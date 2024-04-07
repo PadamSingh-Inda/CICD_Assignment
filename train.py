ed23b2bad32349dcb3f3bc2ff479660ae818927c
Padam Singh <padasing@cisco.com>
2024-04-07 23:21:09 +0530

    test

--- train.py
+++ train.py
@@ -13,12 +13,12 @@ y = df['Disease'].values
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 # Define hyperparameter grids for each classifier
-param_grid_rf = {
-    'n_estimators': [50, 100, 200],
-    'max_depth': [None, 10, 20],
-    'min_samples_split': [2, 5, 10],
-    'min_samples_leaf': [1, 2, 4]
-}
+# param_grid_rf = {
+#     'n_estimators': [50, 100, 200],
+#     'max_depth': [None, 10, 20],
+#     'min_samples_split': [2, 5, 10],
+#     'min_samples_leaf': [1, 2, 4]
+# }
 
 # param_grid_dt = {
 #     'max_depth': [None, 10, 20],
@@ -26,11 +26,17 @@ param_grid_rf = {
 #     'min_samples_leaf': [1, 2, 4]
 # }
 
-# Initialize classifiers
-rf_classifier = RandomForestClassifier()
-# dt_classifier = DecisionTreeClassifier()
+param_grid_adaboost = {
+    'n_estimators': [50, 100, 200],
+    'learning_rate': [0.01, 0.1, 0.2]
+}
 
-grid_search = GridSearchCV(estimator=rf_classifier, param_grid=param_grid_rf, cv=5, scoring='accuracy')
+# Initialize classifiers
+# rf_classifier = RandomForestClassifier()
+# dt_classifier = DecisionTreeClassifier()
+adaboost_classifier = AdaBoostClassifier()
+
+grid_search = GridSearchCV(estimator=adaboost_classifier, param_grid=param_grid_adaboost, cv=5, scoring='accuracy')
 grid_search.fit(X_train, y_train)
 
 best_params = grid_search.best_params_
