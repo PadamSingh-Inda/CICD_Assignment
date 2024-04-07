72c8cb82b2dc254ee12da3d005aeb37579d6e3b1
Padam Singh <padasing@cisco.com>
2024-04-07 23:49:11 +0530

    test

--- train.py
+++ train.py
@@ -1,47 +1,15 @@
 import pickle
+import numpy as np
 import pandas as pd
-from sklearn.model_selection import train_test_split, GridSearchCV
-from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
-from sklearn.tree import DecisionTreeClassifier
+from sklearn.naive_bayes import GaussianNB
 
-# Load data
 df = pd.read_csv("data/train.csv")
-X = df.drop(columns=['Disease']).values
-y = df['Disease'].values
+X = df.drop(columns=['Disease']).to_numpy()
+y = df['Disease'].to_numpy()
+labels = np.sort(np.unique(y))
+y = np.array([np.where(labels == x) for x in y]).flatten()
 
-# Split data into train and test sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+model = GaussianNB().fit(X, y)
 
-# Define hyperparameter grids for each classifier
-# param_grid_rf = {
-#     'n_estimators': [50, 100, 200],
-#     'max_depth': [None, 10, 20],
-#     'min_samples_split': [2, 5, 10],
-#     'min_samples_leaf': [1, 2, 4]
-# }
-
-# param_grid_dt = {
-#     'max_depth': [None, 10, 20],
-#     'min_samples_split': [2, 5, 10],
-#     'min_samples_leaf': [1, 2, 4]
-# }
-
-param_grid_adaboost = {
-    'n_estimators': [50, 100, 200],
-    'learning_rate': [0.01, 0.1, 0.2]
-}
-
-# Initialize classifiers
-# rf_classifier = RandomForestClassifier()
-# dt_classifier = DecisionTreeClassifier()
-adaboost_classifier = AdaBoostClassifier()
-
-grid_search = GridSearchCV(estimator=adaboost_classifier, param_grid=param_grid_adaboost, cv=5, scoring='accuracy')
-grid_search.fit(X_train, y_train)
-
-best_params = grid_search.best_params_
-best_model = grid_search.best_estimator_
-
-# Save the best model
 with open("model.pkl", 'wb') as f:
-    pickle.dump(best_model, f)
+    pickle.dump(model, f)
