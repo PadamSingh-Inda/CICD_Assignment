39121bc7dc0624ca79fdd98e3bf94dff25511350
Padam Singh <padasing@cisco.com>
2024-04-07 16:03:22 +0530

    test

--- train.py
+++ train.py
@@ -11,12 +11,12 @@ df = pd.read_csv("data/train.csv")
 X = df.drop(columns=['Disease']).values
 y = df['Disease'].values
 
-# # Split data into train and test sets
-# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+# Split data into train and test sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 # Define and train AdaBoost model
 adaboost_model = AdaBoostClassifier(n_estimators=10, random_state=42)
-adaboost_model.fit(X, y)
+adaboost_model.fit(X_train, y_train)
 
 # # Evaluate model
 # y_pred = adaboost_model.predict(X_test)
