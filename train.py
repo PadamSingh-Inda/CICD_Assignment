b1b3fc573566b04c6e4183605dbf4d4800eabf21
Padam Singh <padasing@cisco.com>
2024-04-07 23:05:38 +0530

    test

--- train.py
+++ train.py
@@ -1,25 +1,31 @@
-import pandas as pd
-from sklearn.linear_model import LogisticRegression
 import pickle
+import pandas as pd
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.tree import DecisionTreeClassifier
 
 # Load data
 df = pd.read_csv("data/train.csv")
 X = df.drop(columns=['Disease']).values
 y = df['Disease'].values
 
-# Define hyperparameters
-hyperparameters = {
-    'C': 0.1,  # Regularization parameter
-    'solver': 'liblinear',  # Solver for optimization
-    'max_iter': 100  # Maximum number of iterations
+# Split data into train and test sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+param_grid_dt = {
+    'max_depth': [None, 10, 20],
+    'min_samples_split': [2, 5, 10],
+    'min_samples_leaf': [1, 2, 4]
 }
 
-# Initialize logistic regression model with hyperparameters
-logistic_model = LogisticRegression(**hyperparameters)
+# Initialize classifiers
+dt_classifier = DecisionTreeClassifier()
 
-# Train the model
-logistic_model.fit(X, y)
+grid_search = GridSearchCV(estimator=dt_classifier, param_grid=param_grid_dt, cv=5, scoring='accuracy')
+grid_search.fit(X_train, y_train)
+
+best_params = grid_search.best_params_
+best_model = grid_search.best_estimator_
 
 # Save the best model
 with open("model.pkl", 'wb') as f:
-    pickle.dump(logistic_model, f)
+    pickle.dump(best_model, f)
