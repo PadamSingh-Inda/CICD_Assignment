084c2004c7e58106f187c8803261a402bfc159e5
Padam Singh <padasing@cisco.com>
2024-04-07 16:41:57 +0530

    test

--- train.py
+++ train.py
@@ -10,33 +10,33 @@ df = pd.read_csv("data/train.csv")
 X = df.drop(columns=['Disease']).values
 y = df['Disease'].values
 
-# Encoding target variable (if necessary)
-labels = np.sort(np.unique(y))
-y = np.array([np.where(labels == x) for x in y]).flatten()
-
-# Define hyperparameters grid
-param_grid = {
-    'C': [0.001, 0.01, 0.1, 1, 10, 100],  # Regularization parameter
-    'solver': ['newton-cg', 'lbfgs', 'liblinear', 'sag', 'saga'],  # Solver for optimization
-    'max_iter': [100, 200, 300]  # Maximum number of iterations
-}
+# # Encoding target variable (if necessary)
+# labels = np.sort(np.unique(y))
+# y = np.array([np.where(labels == x) for x in y]).flatten()
+#
+# # Define hyperparameters grid
+# param_grid = {
+#     'C': [0.001, 0.01, 0.1, 1, 10, 100],  # Regularization parameter
+#     'solver': ['newton-cg', 'lbfgs', 'liblinear', 'sag', 'saga'],  # Solver for optimization
+#     'max_iter': [100, 200, 300]  # Maximum number of iterations
+# }
 
 # Initialize logistic regression model
-logistic_model = RandomForestRegressor()
+rf = RandomForestRegressor()
 
-# Initialize GridSearchCV
-grid_search = GridSearchCV(estimator=logistic_model, param_grid=param_grid, cv=5, scoring='accuracy')
+# # Initialize GridSearchCV
+# grid_search = GridSearchCV(estimator=logistic_model, param_grid=param_grid, cv=5, scoring='accuracy')
 
 # Perform grid search
-grid_search.fit(X, y)
+rf.fit(X, y)
 
-# Get best hyperparameters and model
-best_params = grid_search.best_params_
-best_model = grid_search.best_estimator_
+# # Get best hyperparameters and model
+# best_params = grid_search.best_params_
+# best_model = grid_search.best_estimator_
 
 # Save the best model
 with open("model.pkl", 'wb') as f:
-    pickle.dump(best_model, f)
+    pickle.dump(rf, f)
 
-print("Best Hyperparameters:", best_params)
-print("Best Accuracy:", grid_search.best_score_)
+# print("Best Hyperparameters:", best_params)
+# print("Best Accuracy:", grid_search.best_score_)
