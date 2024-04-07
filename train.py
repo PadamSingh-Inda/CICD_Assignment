c1d075b4c836470000883f9b490dc1775b4deb44
Padam Singh <padasing@cisco.com>
2024-04-07 16:09:27 +0530

    test

--- train.py
+++ train.py
@@ -1,28 +1,15 @@
 import pandas as pd
-import numpy as np
-from sklearn.model_selection import train_test_split
-from sklearn.ensemble import AdaBoostClassifier
-from sklearn.ensemble import BaggingClassifier, BaggingRegressor
-from sklearn.metrics import accuracy_score
+from sklearn.linear_model import LogisticRegression
 import pickle
+import numpy as np
 
-# Load data
 df = pd.read_csv("data/train.csv")
-X = df.drop(columns=['Disease']).values
-y = df['Disease'].values
+X = df.drop(columns=['Disease']).to_numpy()
+y = df['Disease'].to_numpy()
+labels = np.sort(np.unique(y))
+y = np.array([np.where(labels == x) for x in y]).flatten()
 
-# Split data into train and test sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+model = LogisticRegression().fit(X, y)
 
-# Define and train AdaBoost model
-adaboost_model = AdaBoostClassifier(n_estimators=10, random_state=42)
-adaboost_model.fit(X_train, y_train)
-
-# # Evaluate model
-# y_pred = adaboost_model.predict(X_test)
-# accuracy = accuracy_score(y_test, y_pred)
-# print("Accuracy:", accuracy)
-
-# Save the model
 with open("model.pkl", 'wb') as f:
-    pickle.dump(adaboost_model, f)
+    pickle.dump(model, f)
