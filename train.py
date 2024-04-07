cb9eb3779fdca849955284c944fde84eab7c29f8
Padam Singh <padasing@cisco.com>
2024-04-07 16:00:53 +0530

    test

--- train.py
+++ train.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import AdaBoostClassifier
+from sklearn.ensemble import BaggingClassifier, BaggingRegressor
 from sklearn.metrics import accuracy_score
 import pickle
 
@@ -10,17 +11,17 @@ df = pd.read_csv("data/train.csv")
 X = df.drop(columns=['Disease']).values
 y = df['Disease'].values
 
-# Split data into train and test sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+# # Split data into train and test sets
+# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 # Define and train AdaBoost model
-adaboost_model = AdaBoostClassifier(n_estimators=50, random_state=42)
-adaboost_model.fit(X_train, y_train)
+adaboost_model = AdaBoostClassifier(n_estimators=10, random_state=42)
+adaboost_model.fit(X, y)
 
-# Evaluate model
-y_pred = adaboost_model.predict(X_test)
-accuracy = accuracy_score(y_test, y_pred)
-print("Accuracy:", accuracy)
+# # Evaluate model
+# y_pred = adaboost_model.predict(X_test)
+# accuracy = accuracy_score(y_test, y_pred)
+# print("Accuracy:", accuracy)
 
 # Save the model
 with open("model.pkl", 'wb') as f:
