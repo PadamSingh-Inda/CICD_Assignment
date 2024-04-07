62aa728025cde354628c08871cb3423df9c1ea63
Padam Singh <padasing@cisco.com>
2024-04-07 23:11:03 +0530

    test

--- train.py
+++ train.py
@@ -1,6 +1,7 @@
 import pickle
 import pandas as pd
 from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
 from sklearn.tree import DecisionTreeClassifier
 
 # Load data
@@ -11,16 +12,25 @@ y = df['Disease'].values
 # Split data into train and test sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-param_grid_dt = {
+# Define hyperparameter grids for each classifier
+param_grid_rf = {
+    'n_estimators': [50, 100, 200],
     'max_depth': [None, 10, 20],
     'min_samples_split': [2, 5, 10],
     'min_samples_leaf': [1, 2, 4]
 }
 
-# Initialize classifiers
-dt_classifier = DecisionTreeClassifier()
+# param_grid_dt = {
+#     'max_depth': [None, 10, 20],
+#     'min_samples_split': [2, 5, 10],
+#     'min_samples_leaf': [1, 2, 4]
+# }
 
-grid_search = GridSearchCV(estimator=dt_classifier, param_grid=param_grid_dt, cv=5, scoring='accuracy')
+# Initialize classifiers
+rf_classifier = RandomForestClassifier()
+# dt_classifier = DecisionTreeClassifier()
+
+grid_search = GridSearchCV(estimator=rf_classifier, param_grid=param_grid_rf, cv=5, scoring='accuracy')
 grid_search.fit(X_train, y_train)
 
 best_params = grid_search.best_params_
